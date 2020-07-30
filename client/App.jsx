80fe836b798c5bf412c7e2b09233ffd16ed99558
xApnea <jcbruno150@gmail.com>
2020-07-30 14:09:54 -0400

    Pin selector UI is working and passing state upstream

--- client/App.jsx
+++ client/App.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import axios from 'axios';
+import PinSelector from './PinSelector.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      pinsToHit: 0
     }
+    this.updatePinsToHit = this.updatePinsToHit.bind(this);
   }
 
-componentDidMount() {
-  axios.get('/api')
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-}
+  componentDidMount() {
+    axios.get('/api')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
+
+  updatePinsToHit(event) {
+    const pinsToHit = parseInt(event.target.value);
+    this.setState({pinsToHit: pinsToHit});
+  }
 
   render () {
     return (
       <div>
         <h1>Bowling</h1>
+        <PinSelector handleButtonClick={this.updatePinsToHit}/>
     </div>
     )
   }
---
+++ client/PinSelector.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class PinSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+  render() {
+    return(
+      <div>
+        <h2>Select Number of Pins</h2>
+        <div>
+          <div>
+            <button onClick={this.props.handleButtonClick} value={1}>1</button>
+            <button onClick={this.props.handleButtonClick} value={2}>2</button>
+            <button onClick={this.props.handleButtonClick} value={3}>3</button>
+          </div>
+          <div>
+            <button onClick={this.props.handleButtonClick} value={4}>4</button>
+            <button onClick={this.props.handleButtonClick} value={5}>5</button>
+            <button onClick={this.props.handleButtonClick} value={6}>6</button>
+          </div>
+          <div>
+            <button onClick={this.props.handleButtonClick} value={7}>7</button>
+            <button onClick={this.props.handleButtonClick} value={8}>8</button>
+            <button onClick={this.props.handleButtonClick} value={9}>9</button>
+          </div>
+          <div>
+            <button onClick={this.props.handleButtonClick} value={10}>10</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PinSelector;
